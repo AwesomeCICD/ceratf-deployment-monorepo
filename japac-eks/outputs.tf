0e6ed34342479b4b3c84ef94919b751a6091e66c
Derry Bradley <derrybradley@live.ie>
2023-10-26 17:32:20 +0200

    trigger japac eks layer to plan

---
+++ japac-eks/outputs.tf
@@ -0,0 +1,52 @@
+output "kubeconfig_update_command" {
+  value = module.se_eks_cluster.kubeconfig_update_command
+}
+
+output "circleci_region" {
+  value = local.circleci_region
+}
+
+output "cluster_name" {
+  value = module.se_eks_cluster.cluster_name
+}
+
+output "cluster_endpoint" {
+  value     = module.se_eks_cluster.cluster_endpoint
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = module.se_eks_cluster.cluster_ca_certificate
+  sensitive = true
+}
+
+output "target_domain" {
+  value = module.regional_dns.r53_subdomain_zone_name
+}
+
+output "cluster_oidc_provider_arn" {
+  value = module.se_eks_cluster.oidc_provider_arn
+}
+output "vault_namespace" {
+  value = module.vault.namespace
+}
+
+/*
+#For Kubernetes custom resources
+output "cluster_endpoint" {
+  value = module.se_eks_cluster.cluster_endpoint
+}
+
+output "cluster_name" {
+  value = module.se_eks_cluster.cluster_name
+}
+
+output "cluster_ca_certificate" {
+  value = module.se_eks_cluster.cluster_ca_certificate
+}
+
+
+output "r53_subdomain_zone_id" {
+  value = module.regional_dns.r53_subdomain_zone_id
+}
+*/
