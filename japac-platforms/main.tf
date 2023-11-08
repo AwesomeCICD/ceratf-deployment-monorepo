b253010599b834b78435269d3f824006a9bd1ca9
Eddie Webb <ollitech@gmail.com>
2023-11-08 11:04:01 -0500

    add dr-demo ns to agent in all clusters

--- japac-platforms/main.tf
+++ japac-platforms/main.tf
@@ -38,5 +38,7 @@ module "release_agent" {
 
   chart_version = "0.0.5"
 
+  managed_namespaces = ["default", "guidebook", "guidebook-dev", "boa", "boa-dev", "circleci-release-agent-system", "dr-demo"]
+
   depends_on = [module.argo_rollouts]
 }
