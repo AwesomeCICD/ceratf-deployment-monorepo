9facee45239ac9dadfbd0ea99137e0ccec673af4
David Castillo <d.v.castillo37@gmail.com>
2023-11-17 10:52:31 -0600

    update to the lastest version of appspaces

--- japac-platforms/main.tf
+++ japac-platforms/main.tf
@@ -22,7 +22,7 @@ module "nexus_config" {
 }
 
 module "app_spaces" {
-  source           = "git@github.com:AwesomeCICD/ceratf-module-appspaces?ref=1.2.0"
+  source           = "git@github.com:AwesomeCICD/ceratf-module-appspaces?ref=1.3.0"
   cluster_endpoint = data.terraform_remote_state.ceratf_regional.outputs.cluster_endpoint
   cluster_name     = data.terraform_remote_state.ceratf_regional.outputs.cluster_name
 }
