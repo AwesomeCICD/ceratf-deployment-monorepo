5aa543683a392f6e922bd2da77ff3ce221d4189d
Eddie Webb <ollitech@gmail.com>
2024-01-19 10:43:23 -0500

    always run groovy

--- emea-platforms/main.tf
+++ emea-platforms/main.tf
@@ -18,7 +18,7 @@ module "nexus" {
 }
 
 module "nexus_config" {
-  source     = "git@github.com:AwesomeCICD/ceratf-module-nexus-config?ref=0.1.4"
+  source     = "git@github.com:AwesomeCICD/ceratf-module-nexus-config?ref=0.1.5"
   depends_on = [module.nexus]
 }
 
