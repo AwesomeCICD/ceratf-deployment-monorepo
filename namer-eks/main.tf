a0c942e157bc64d04b0ea11e82475bc299830796
Eddie Webb <ollitech@gmail.com>
2024-06-21 16:21:05 -0400

    eks 2.0.2 with ebs fix

--- namer-eks/main.tf
+++ namer-eks/main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "fe_eks_cluster" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=2.0.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=2.0.2"
 
   cluster_version                = "1.30"
   cluster_suffix                 = local.circleci_region
