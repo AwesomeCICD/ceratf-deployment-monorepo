9a38fb9f7a33ec2aacc70ae10c42f90fab5ddb35
Eddie Webb <ollitech@gmail.com>
2024-06-21 12:47:47 -0400

    tfmt

--- namer-eks/main.tf
+++ namer-eks/main.tf
@@ -9,11 +9,11 @@ locals {
 module "fe_eks_cluster" {
   source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=2.0.0"
 
-  cluster_version            = "1.30"
-  cluster_suffix             = local.circleci_region
-  node_instance_types        = ["m5a.xlarge"]
-  nodegroup_desired_capacity = 2
-  cluster_endpoint_public_access  = true
+  cluster_version                = "1.30"
+  cluster_suffix                 = local.circleci_region
+  node_instance_types            = ["m5a.xlarge"]
+  nodegroup_desired_capacity     = 2
+  cluster_endpoint_public_access = true
 }
 
 module "regional_dns" {
