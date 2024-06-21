70db6f8982be46814e180a26a299e8392fb52d49
Eddie Webb <ollitech@gmail.com>
2024-06-21 18:32:07 -0400

    try ssl fix in namer

--- emea-eks/main.tf
+++ emea-eks/main.tf
@@ -8,7 +8,7 @@ locals {
 
 
 module "fe_eks_cluster" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=2.0.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=2.0.3"
 
   cluster_version                = "1.30"
   cluster_suffix                 = local.circleci_region
@@ -29,7 +29,7 @@ module "regional_dns" {
 # A bit odd to see istio here, but it includes cert-manager whcih interacts with KMS and Istio creates ELBs 
 # that will prevent this plan from destorying cluster (AWS blocks delete since networkinterface is attached)
 module "helm_istio" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-helm-istio.git?ref=2.0.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-helm-istio.git?ref=2.1.0"
 
   aws_region                = data.aws_region.current.name
   aws_account_no            = data.aws_caller_identity.current.account_id
@@ -38,6 +38,7 @@ module "helm_istio" {
   node_security_group_id    = module.fe_eks_cluster.node_security_group_id
   circleci_region           = local.circleci_region
   root_domain_zone_id       = data.terraform_remote_state.ceratf_deployment_global.outputs.r53_root_zone_id
+  root_domain_zone_name       = data.terraform_remote_state.ceratf_deployment_global.outputs.r53_root_zone_name
   target_domain             = module.regional_dns.r53_subdomain_zone_name
   r53_subdomain_zone_id     = module.regional_dns.r53_subdomain_zone_id
   cluster_oidc_provider_arn = module.fe_eks_cluster.oidc_provider_arn
