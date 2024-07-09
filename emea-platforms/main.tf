71bf85b879fd722958e41b2d27b06b6dc548dd2b
Eddie Webb <ollitech@gmail.com>
2024-07-09 09:04:22 -0400

    add new cba namespace and policy

--- emea-platforms/main.tf
+++ emea-platforms/main.tf
@@ -8,7 +8,7 @@ locals {
 
 
 module "vault_config" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.8.1"
+  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.9.0"
 }
 
 module "nexus" {
@@ -26,7 +26,7 @@ module "nexus_config" {
 
 
 module "app_spaces" {
-  source           = "git@github.com:AwesomeCICD/ceratf-module-appspaces?ref=1.4.0"
+  source           = "git@github.com:AwesomeCICD/ceratf-module-appspaces?ref=1.5.0"
   cluster_endpoint = data.terraform_remote_state.ceratf_regional.outputs.cluster_endpoint
   cluster_name     = data.terraform_remote_state.ceratf_regional.outputs.cluster_name
 }
