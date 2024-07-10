d99723f45753f75dc3af56dfb0b286445faeb55e
your_github_username <nitin@circleci.com>
2024-07-10 10:57:26 -0700

    pushing unsaved changes

--- global/providers.tf
+++ global/providers.tf
@@ -7,16 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-<<<<<<< HEAD
     #profile        = "jennings-dev-ccidev"
     bucket         = "fe-tf-cluster-capitalone"
     region         = "us-east-1"
     key            = "se-eks-cluster/global/terraform.tfstate"
-=======
-    bucket         = "fe-cluster-tf-state"
-    region         = "us-west-2"
-    key            = "fe-eks-cluster/global/terraform.tfstate"
->>>>>>> fe-account
     dynamodb_table = "cera-tf-lock"
   }
 }
