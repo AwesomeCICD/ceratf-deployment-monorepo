3ee47f0898e646b240302d29456d128e7ee9cb41
your_github_username <nitin@circleci.com>
2024-07-10 12:28:28 -0700

    Updating bucket name

--- global/s3.tf
+++ global/s3.tf
@@ -2,10 +2,10 @@
 
 import {
   to = aws_s3_bucket.capitalone_s3_state_bucket
-  id = "fe-cluster-tf-state"
+  id = "fe-tf-cluster-capitalone"
 }
 
 resource "aws_s3_bucket" "capitalone_s3_state_bucket" {
-  bucket = "fe-cluster-tf-state-capitalone"
+  bucket = "fe-tf-cluster-capitalone"
   tags   = var.common_tags
 }
