3a2f63407ca93a55d2f909ae0ced8d473ce27f47
your_github_username <nitin@circleci.com>
2024-07-10 13:17:45 -0700

    removing bucket import

--- global/s3.tf
+++ global/s3.tf
@@ -1,9 +1,9 @@
 
 
-import {
-  to = aws_s3_bucket.capitalone_s3_state_bucket
-  id = "fe-tf-cluster-capitalone"
-}
+#import {
+ # to = aws_s3_bucket.capitalone_s3_state_bucket
+  #id = "fe-tf-cluster-capitalone"
+#}
 
 resource "aws_s3_bucket" "capitalone_s3_state_bucket" {
   bucket = "fe-tf-cluster-capitalone"
