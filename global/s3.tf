6e3776138c74f63fa6bf5e03a3214420634b6445
Vijay Pandian <ananth.vijay@gmail.com>
2024-07-10 14:51:48 -0400

    use unique bucket name and pin it to us-east-1

--- global/s3.tf
+++ global/s3.tf
@@ -6,6 +6,7 @@ import {
 }
 
 resource "aws_s3_bucket" "capitalone_s3_state_bucket" {
-  bucket = "fe-cluster-tf-state"
-  tags   = var.common_tags
+  bucket                      = "fe-cluster-tf-state-capitalone"
+  bucket_regional_domain_name = "s3.us-east-1.amazonaws.com"
+  tags                        = var.common_tags
 }
