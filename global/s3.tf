fe8383875e812780e3a00c04eb2a0e0767705cab
Vijay Pandian <ananth.vijay@gmail.com>
2024-07-10 14:44:10 -0400

    import existing bucket

--- global/s3.tf
+++ global/s3.tf
@@ -1,5 +1,10 @@
 
 
+import {
+  to = aws_s3_bucket.capitalone_s3_state_bucket
+  id = "fe-cluster-tf-state"
+}
+
 resource "aws_s3_bucket" "capitalone_s3_state_bucket" {
   bucket = "fe-cluster-tf-state"
   tags   = var.common_tags
