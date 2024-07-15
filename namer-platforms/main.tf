71b4c873e533f9cec85f34d80bbdbead67cd3ff2
Jim Crowley <42618816+james-crowley@users.noreply.github.com>
2024-07-15 15:34:02 -0400

    Updated vault config for ecap

--- namer-platforms/main.tf
+++ namer-platforms/main.tf
@@ -9,7 +9,7 @@ locals {
 
 
 module "vault_config" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.9.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.10.0"
 }
 
 module "nexus" {
