b637da1b3cfd05877e22b6e72bd3a17e2f23e4b6
Jim Crowley <42618816+james-crowley@users.noreply.github.com>
2024-07-17 11:53:37 -0400

    Update main.tf

--- namer-platforms/main.tf
+++ namer-platforms/main.tf
@@ -9,7 +9,7 @@ locals {
 
 
 module "vault_config" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.9.4"
+  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.9.5"
 }
 
 module "nexus" {
