8d8ec9cd440f2667a0020fcc20a9ca5473145ddf
Eddie Webb <ollitech@gmail.com>
2024-07-22 17:08:31 -0400

    nexus pvc pinned to az b

--- namer-eks/main.tf
+++ namer-eks/main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "fe_eks_cluster" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=4.0.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-eks.git?ref=5.0.0"
 
   cluster_version                = "1.30"
   cluster_suffix                 = local.circleci_region
