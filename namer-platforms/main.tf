ce0d118249ee10a7c24a9a823161c018ed3fa698
Eddie Webb <ollitech@gmail.com>
2024-09-26 16:55:36 -0400

    use 1.3.1 CCI Release Agent supporting unique naming

--- namer-platforms/main.tf
+++ namer-platforms/main.tf
@@ -39,7 +39,7 @@ module "argo_rollouts" {
 
 
 module "release_agent" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-helm-cci-release-agent?ref=1.3.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-helm-cci-release-agent?ref=1.3.1"
 
   release_agent_token = var.rt_token
 
@@ -50,7 +50,7 @@ module "release_agent" {
 
 
 module "release_agent_dev" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-helm-cci-release-agent?ref=1.3.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-helm-cci-release-agent?ref=1.3.1"
 
   release_agent_token = var.rt_token_dev
 
