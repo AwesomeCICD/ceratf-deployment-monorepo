15523b9020b3fd7cf5b167c0215920f064c669b3
Eddie Webbinaro <eddie@circleci.com>
2025-03-12 11:12:15 -0400

    rev vault and appspaces for circle-shop (#29)
    * rev vault and appspaces for circle-shop
    
    * try awesomecicd-deploybot ssh key
    
    * try awesomecicd-deploybot ssh key
    
    * update readme

--- namer-platforms/main.tf
+++ namer-platforms/main.tf
@@ -8,7 +8,7 @@ locals {
 
 
 module "vault_config" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.12.1"
+  source = "git@github.com:AwesomeCICD/ceratf-module-vault-config?ref=1.13.0"
 }
 
 
@@ -28,7 +28,7 @@ module "nexus_config" {
 
 
 module "app_spaces" {
-  source           = "git@github.com:AwesomeCICD/ceratf-module-appspaces?ref=3.3.0"
+  source           = "git@github.com:AwesomeCICD/ceratf-module-appspaces?ref=3.4.0"
   cluster_endpoint = data.terraform_remote_state.ceratf_regional.outputs.cluster_endpoint
   cluster_name     = data.terraform_remote_state.ceratf_regional.outputs.cluster_name
 }
