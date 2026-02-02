f7b253ef595635a6c3f8b1e9d318821da10f8b47
Nitin Kuriakose <nitin@circleci.com>
2026-02-02 02:26:41 -0800

    chore: update critical_until tag to 2026 and trigger global apply for partner product ID tag

--- global/variables.tf
+++ global/variables.tf
@@ -41,7 +41,7 @@ variable "common_tags" {
     "Team"                = "Field Engineering"
     "iac"                 = "true"
     "optimization_opt_in" = "true"
-    "critical_until"      = "2024-12-31"
+    "critical_until"      = "2026-12-31"
     "data_classification" = "low"
     "purpose"             = "CERA is a customer facing demo architecture used by Field Engineering team."
   }
