2a7e4d0a1867a28626141d51e8ea93473e4081bb
Derry Bradley <derrybradley@live.ie>
2026-02-13 12:18:07 +0000

    clean up providers

--- namer-platforms/main.tf
+++ namer-platforms/main.tf
@@ -85,7 +85,7 @@ module "authentik" {
 
 
 module "grafana" {
-  source = "git@github.com:AwesomeCICD/ceratf-module-helm-grafana.git?ref=2.0.0"
+  source = "git@github.com:AwesomeCICD/ceratf-module-helm-grafana.git?ref=2.0.1"
 
   namespace    = "monitoring"
   release_name = "grafana"
